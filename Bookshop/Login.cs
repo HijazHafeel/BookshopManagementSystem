cddde2efed7a2fd8d03e2f2c5b92ef964e9ae73e
HijazHafeel <hijazmh-ec22049@stu.kln.ac.lk>
2026-05-23 21:46:15 +0530

    Update Login.cs

--- Bookshop/Login.cs
+++ Bookshop/Login.cs
@@ -16,5 +16,45 @@ namespace Bookshop_Management_System.Bookshop
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblUname_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDisp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void llblFPd_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
     }
 }
